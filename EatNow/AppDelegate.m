e3825815de1f20250e8f270ac6f6fa06c5c58d31
GaoYongqing <gaoyq@live.cn>
2015-11-21 16:12:36 +0800

    Fix fabric issue

--- EatNow/AppDelegate.m
+++ EatNow/AppDelegate.m
@@ -31,12 +31,12 @@
 #import "ATConnect.h"
 #import "UIImageView+AFNetworking.h"
 #import "WatchKitAction.h"
-//#import "CrashlyticsLogger.h"
-//#import <Fabric/Fabric.h>
+#import "CrashlyticsLogger.h"
+#import <Fabric/Fabric.h>
 #import "DDLog.h"
 #import "DDASLLogger.h"
 #import "DDTTYLogger.h"
-//#import <Crashlytics/crashlytics.h>
+#import <Crashlytics/crashlytics.h>
 #import "ENLostConnectionViewController.h"
 #import "Mixpanel.h"
 #import "BlocksKit+UIKit.h"
@@ -67,8 +67,8 @@
     //plugin init
     [ATConnect sharedConnection].apiKey = @"43aadd17c4e966f98753bcb1250e78d00c68731398a9b60dc7c456d2682415fc";
     [ATConnect sharedConnection].appID = @"946591471";
-    //[Fabric with:@[CrashlyticsKit]];
-    //[Fabric sharedSDK].debug = YES;
+    [Fabric with:@[CrashlyticsKit]];
+    [Fabric sharedSDK].debug = YES;
     [Mixpanel sharedInstanceWithToken:@"c75539720b4a190037fd1d4f0d9c7a56"];
     
     //Wechat
